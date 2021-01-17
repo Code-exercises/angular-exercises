b232a9083c2f1ab8640365f82fff8e229e70b13d
Roman Cherepanov <romach007@gmail.com>
2021-01-17 02:12:15 +0200

    change exercise creation script

--- create-exercise.sh
+++ create-exercise.sh
@@ -5,3 +5,4 @@ mkdir -p "$EXERCISE_NAME"/1-finish/
 rsync -av exercise-template/0-start/ "$EXERCISE_NAME"/0-start/
 echo "# $EXERCISE_NAME" > "$EXERCISE_NAME"/0-start/README.md
 rsync -av "$EXERCISE_NAME"/0-start/ "$EXERCISE_NAME"/1-finish/
+git add "$EXERCISE_NAME"
