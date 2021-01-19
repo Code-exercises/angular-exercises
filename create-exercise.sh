310dd53c54f674a46ad8d73ce7954d9f8a375149
Roman Cherepanov <romach007@gmail.com>
2021-01-19 20:33:45 +0200

    Update script for creating exercise

--- create-exercise.sh
+++ create-exercise.sh
@@ -2,7 +2,7 @@
 EXERCISE_NAME="$1"
 mkdir -p "$EXERCISE_NAME"/0-start/
 mkdir -p "$EXERCISE_NAME"/1-finish/
-rsync -av exercise-template/0-start/ "$EXERCISE_NAME"/0-start/
+rsync -av exercise-template/app/ "$EXERCISE_NAME"/0-start/
 echo "# $EXERCISE_NAME" > "$EXERCISE_NAME"/0-start/README.md
 rsync -av "$EXERCISE_NAME"/0-start/ "$EXERCISE_NAME"/1-finish/
 git add "$EXERCISE_NAME"
